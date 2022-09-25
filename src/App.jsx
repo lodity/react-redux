e239fc13c79c4546cd783fcb1fc077b6e0f21edb
lodity <lodity9@gmail.com>
2022-09-25 12:02:00 +0300

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+	addCustomerAction,
+	removeCustomerAction,
+} from './store/customerReducer';
 
 function App() {
 	const dispatch = useDispatch();
 	const cash = useSelector((state) => state.cash.cash);
+	const customers = useSelector((state) => state.customers.customers);
 
 	const addCash = (cash) => {
 		dispatch({ type: 'ADD_CASH', payload: cash });
@@ -12,6 +17,18 @@ function App() {
 
 	const getCash = (cash) => {
 		dispatch({ type: 'GET_CASH', payload: cash });
+	};
+
+	const addCustomer = (name) => {
+		const customer = {
+			name,
+			id: Date.now(),
+		};
+		dispatch(addCustomerAction(customer));
+	};
+
+	const removeCustomer = (customer) => {
+		dispatch(removeCustomerAction(customer.id));
 	};
 
 	return (
@@ -24,7 +41,33 @@ function App() {
 				<button onClick={() => getCash(Number(prompt()))}>
 					Снять со счёта
 				</button>
+				<button onClick={() => addCustomer(prompt())}>
+					Добавить клиента
+				</button>
+				<button onClick={() => getCash(Number(prompt()))}>
+					Удалить клиента
+				</button>
 			</div>
+			{customers.length > 0 ? (
+				<div>
+					{customers.map((customer) => (
+						<div
+							onClick={() => removeCustomer(customer)}
+							style={{
+								fontSize: '2rem',
+								padding: '10px',
+								border: '1px solid black',
+								display: 'inline-block',
+								marginTop: '10px',
+							}}
+						>
+							{customer.name}
+						</div>
+					))}
+				</div>
+			) : (
+				<h2>There are no customers</h2>
+			)}
 		</div>
 	);
 }
