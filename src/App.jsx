81fbbc5bf5ce8c73bb5ff7d92086ae5e7a1b5741
lodity <lodity9@gmail.com>
2022-09-25 22:21:59 +0300

    troubles with git fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,72 +2,42 @@ import React from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-	addCustomerAction,
-	removeCustomerAction,
-} from './store/customerReducer';
+	asyncDecrementCreator,
+	asyncIncrementCreator,
+} from './store/countReducer';
 
 function App() {
 	const dispatch = useDispatch();
-	const cash = useSelector((state) => state.cash.cash);
-	const customers = useSelector((state) => state.customers.customers);
-
-	const addCash = (cash) => {
-		dispatch({ type: 'ADD_CASH', payload: cash });
-	};
-
-	const getCash = (cash) => {
-		dispatch({ type: 'GET_CASH', payload: cash });
-	};
-
-	const addCustomer = (name) => {
-		const customer = {
-			name,
-			id: Date.now(),
-		};
-		dispatch(addCustomerAction(customer));
-	};
-
-	const removeCustomer = (customer) => {
-		dispatch(removeCustomerAction(customer.id));
-	};
+	const count = useSelector((state) => state.count.count);
+	const users = useSelector((state) => state.users.users);
 
 	return (
 		<div className="App">
-			<div style={{ fontSize: 48 }}>{cash}</div>
+			<div style={{ fontSize: 48 }}>{count}</div>
 			<div style={{ display: 'flex' }}>
-				<button onClick={() => addCash(Number(prompt()))}>
-					Пополнить счёт
+				<button onClick={() => dispatch(asyncIncrementCreator())}>
+					INCREMENT
 				</button>
-				<button onClick={() => getCash(Number(prompt()))}>
-					Снять со счёта
+				<button onClick={() => dispatch(asyncDecrementCreator())}>
+					DECREMENT
 				</button>
-				<button onClick={() => addCustomer(prompt())}>
-					Добавить клиента
-				</button>
-				<button onClick={() => getCash(Number(prompt()))}>
-					Удалить клиента
-				</button>
+				<button>Получить юзеров</button>
 			</div>
-			{customers.length > 0 ? (
-				<div>
-					{customers.map((customer) => (
-						<div
-							onClick={() => removeCustomer(customer)}
-							style={{
-								fontSize: '2rem',
-								padding: '10px',
-								border: '1px solid black',
-								display: 'inline-block',
-								marginTop: '10px',
-							}}
-						>
-							{customer.name}
-						</div>
-					))}
-				</div>
-			) : (
-				<h2>There are no customers</h2>
-			)}
+			<div>
+				{users.map((user) => (
+					<div
+						style={{
+							fontSize: '2rem',
+							padding: '10px',
+							border: '1px solid black',
+							display: 'inline-block',
+							marginTop: '10px',
+						}}
+					>
+						{user.name}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,16 @@
-import { createStore, combineReducers } from 'redux';
-import { cashReducer } from './cashReducer';
-import { customerReducer } from './customerReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { countReducer } from './countReducer';
+import { userReducer } from './userReducer';
+import createSagaMiddleware from 'redux-saga';
+import { countWatcher } from '../saga/countSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer = combineReducers({
-	cash: cashReducer,
-	customers: customerReducer,
+	count: countReducer,
+	users: userReducer,
 });
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(countWatcher);
