28c8c6d1466fb7691bc12be6443919155156ed8c
lodity <lodity9@gmail.com>
2022-09-25 10:21:26 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  return <div className="App"></div>;
+	const dispatch = useDispatch();
+	const cash = useSelector((state) => state.cash);
+
+	const addCash = (cash) => {
+		dispatch({ type: 'ADD_CASH', payload: cash });
+	};
+
+	const getCash = (cash) => {
+		dispatch({ type: 'GET_CASH', payload: cash });
+	};
+
+	return (
+		<div className="App">
+			<div style={{ fontSize: 48 }}>{cash}</div>
+			<div style={{ display: 'flex' }}>
+				<button onClick={() => addCash(Number(prompt()))}>
+					Пополнить счёт
+				</button>
+				<button onClick={() => getCash(Number(prompt()))}>
+					Снять со счёта
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
